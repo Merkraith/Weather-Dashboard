7604b04ddcce92a5f9b59eb6155deadb0b536e3d
Mark Heil <mark.heil1122@gmail.com>
2020-05-30 23:34:25 -0700

    overall almost complete, have not done a commit in a while, still a bug or two

--- js/script.js
+++ js/script.js
@@ -1,39 +1,46 @@
-let city = $(".city");
-let wind = $(".wind");
-let humidity = $(".humidity");
-let temp = $(".temp");
-
 let searchArr = [];
-let APIKey = "&appid=de6bb623e95b6c13d55f60f81ac75405";
+let APIKey = "&appid=de6bb623e95b6c13d55f60f81ac75405"
 
 $(document).ready(function () {
+
+    storeCity();
 
     $("#search-button").click(function (event) {
         event.preventDefault();
 
-        //grab search term from input search field
         let searchTerm = $("#city-input").val().trim();
-        // construct the URL
+        clickSearch(searchTerm);
+        // searchArr.push(searchTerm);
+
+    })
+
+    function clickSearch(searchTerm) {
+
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + APIKey;
 
-        // add name of search term to search array 
-        searchArr.push(searchTerm);
 
-        // add search term to top of list of cities
-        $(`<button class="city-button">`).text(searchTerm).prepend(".stored-cities");
-
+        // creating buttons of every city search
+        $(`<button class="city-button">`).appendTo(".stored-cities").prepend(searchTerm);
         // ajax call for local weather
         $.ajax({
             type: "GET",
             url: queryURL
         }).then(function (response) {
+
+            let previousCity = JSON.parse(localStorage.getItem("cities"));
+            if (previousCity) {
+                previousCity.push(response.name);
+                localStorage.setItem("cities", JSON.stringify(previousCity));
+            } else {
+                searchArr.push(response.name)
+                localStorage.setItem("cities", JSON.stringify(searchArr));
+            }
             $("h1").text(JSON.stringify(response));
-            //log queryURL
-            console.log(queryURL);
-            //log the resulting object
-            console.log(response);
+            let currentDate = moment().format('l');
+
+
             //transfer content to HTML
-            let cityName = $("#current-city").text(searchTerm + " Weather Details");
+            let cityName = $("<h1>").text(searchTerm + " " + "(" + currentDate + ")");
             let windData = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
             let humidityData = $("<p>").text("Humidity: " + response.main.humidity + "%");
             let iconcode = response.weather[0].icon;
@@ -45,17 +52,16 @@ $(document).ready(function () {
             let roundedTemp = Math.floor(tempF);
 
             // temp elements added to html
-            let tempData = $("<p>").text("Temp (K): " + response.main.temp + "°");
             let tempDataF = $("<p>").text("Temp (F): " + roundedTemp + "°");
 
             // append the elements together
-            cityName.append(weatherImg, windData, humidityData, tempData, tempDataF);
-
-            $("container").append(cityName);
+            cityName.append(weatherImg, windData, humidityData, tempDataF);
 
 
-            // creating buttons of every city search
-            $("<p>").appendTo(".stored-cities").prepend(searchTerm);
+            $("#current-city").append(cityName);
+
+
+
 
             let lat = response.coord.lat;
             let lon = response.coord.lon;
@@ -64,44 +70,98 @@ $(document).ready(function () {
             let UVIndexURL = "http://api.openweathermap.org/data/2.5/uvi?" + APIKey + "&lat=" + lat + "&lon=" + lon;
 
             $.ajax({
-                type: "GET",
+                method: "GET",
                 url: UVIndexURL,
-            }).then(function (responseUVIndex) {
-                console.log(responseUVIndex);
+            }).then(function (currentUV) {
+                let displayUV = $("<p id='uv-index'>").text("UV Index: " + currentUV.value);
+                $("#current-city").append(displayUV)
 
-                let currentUV = $("#uv-index").text("UV Index: " + responseUVIndex.value);
-
-                if (currentUV >= 0 && currentUV < 3) {
+                // CSS changes depending on uvindex value
+                if (currentUV.value >= 0 && currentUV.value < 3) {
                     $("#uv-index").addClass("badge-success");
-                } else if (currentUV >= 3 && currentUV < 8) {
+                } else if (currentUV.value >= 3 && currentUV.value < 8) {
                     $("#uv-index").addClass("badge-warning");
-                } else if (currentUV >= 8) {
+                } else if (currentUV.value >= 8) {
                     $("#uv-index").addClass("badge-danger");
                 }
-                $("container").append(currentUV)
             })
 
-
-
-
-            // AJAX call for Five Day Forecast
+            // AJAX url and call for Five Day Forecast
             let fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" + APIKey;
 
             $.ajax({
-                type: "GET",
+                method: "GET",
                 url: fiveDayQueryURL,
-            }).then(function (response5Day) {
-                console.log(response5Day);
-                // console.log(response5Day.list[0].main.temp)
-
-                //     for (var i = 0; i < 6; i++) {
-
-                //     }
-                // })
+            }).then(function (responseFiveDay) {
+                console.log(responseFiveDay);
 
 
+                // for loop for creating 5 cards for a 5 day forecast
+                // let  = response5Day.daily;
+                for (let i = 1; i < 6; i++) {
+                    let fiveDayElement = $("<div class='card bg-primary' style = width: 10rem; margin: 10px;'>")
+                    let fiveDayDate = responseFiveDay.daily[i].dt;
+                    let date = fiveDayDate;
+                    let fiveDayTemp = responseFiveDay.daily[i].temp.day;
+                    let fiveDayHumid = responseFiveDay.daily[i].humidity;
+                    let fiveDayCard = $("<div class='card-body bg-primary'>");
+                    let fiveDayDateEl = $("<h5 class='card-title bg-primary'>");
+                    let fiveDayTempEl = $("<p class='card-text bg-primary'>").text("Temp: " + fiveDayTemp + "° F");
+                    let fiveDayHumEl = $("<p class='card-text bg-primary'>").text("Humidity: " + fiveDayHumid + "%");
+                    let fiveDayIcon = "https://openweathermap.org/img/wn/" + responseFiveDay.daily[i].weather[0].icon + "@2x.png"
+                    let fiveDayImg = $("<img>");
+                    fiveDayImg.attr("src", fiveDayIcon);
+                    fiveDayCard.append(fiveDayDateEl).text("Date: " + moment.unix(fiveDayDate).format("MM/DD/YYYY"));
+                    fiveDayCard.append(fiveDayImg);
+                    fiveDayCard.append(fiveDayTempEl);
+                    fiveDayCard.append(fiveDayHumEl);
+                    fiveDayElement.append(fiveDayCard);
+                    $("#five-day").append(fiveDayElement);
+                }
             });
 
         });
+    }
+    $(document).on("click", ".city-button", function () {
+        JSON.parse(localStorage.getItem("cities"));
+        let citySearch = $(this).text();
+        clickSearch(citySearch);
     });
+
+    function storeCity() {
+        let searchList = JSON.parse(localStorage.getItem("cities"));
+        $(".stored-cities").empty();
+        if (searchList) {
+            for (i = 0; i < searchList.length; i++) {
+                let listBtn = $(`<button class="city-button">`).attr('id', 'cityname_' + (i + 1)).text(searchList[i]);
+                let listElem = $("<li>").attr('class', 'list-group-item');
+                listElem.append(listBtn);
+                $(".stored-cities").append(listElem);
+            }
+
+        }
+
+    }
 });
+
+
+
+// localStorage.setItem(searchTerm, JSON.stringify(searchArr));
+// for (let c = 0; c < searchArr.length; c++) {
+//     $(`<button class="city-button">`).text(searchTerm).prepend(".stored-cities");
+// }
+
+    // let lastCity = JSON.parse(localStorage.getItem(searchTerm));
+    // let storedList = $(`<button class="city-button">`).text(lastCity);
+    // console.log(lastCity)
+    // if (lastCity === null) {
+    //     $(".list-btn").hide();
+    //     console.log($("#city-button"))
+    // } else {
+    //     $(".list-btn").show();
+
+    //     $("#city-list").append(storedList);
+    //     weatherSearch(lastCity);
+    // }
+
+
